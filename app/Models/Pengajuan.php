e5868e95ea4fb736b0658867c4b388c2831c196a
RyanZufar <ryanzufar2005@gmail.com>
2025-10-07 19:28:21 +0700

    merubah bagian app untuk staf fakultas

--- app/Models/Pengajuan.php
+++ app/Models/Pengajuan.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pengajuan extends Model
 {
     use HasFactory;
-    protected $table = 'pengajuan'; 
+    protected $table = 'pengajuan';
     protected $primaryKey = 'pengajuan_id';
     
     public function user() {
